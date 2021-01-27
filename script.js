7c5b1de6a439256a82d6f9c333f0b84f7ffacd65
noheropage <mursepage@gmail.com>
2021-01-26 23:10:12 -0800

    Display current time

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // TODO: display current day at top of calender
-
+// const { DateTIme } = require("luxon");
+var DateTime = luxon.DateTime;
+var dt = DateTime.local()
+var now = DateTime.local()
+console.log(now);
+$("#currentDay").append(dt.toLocaleString(DateTime.DATETIME_MED))
 // TODO: color coded time blocks to indicate past, present, or future
 
 // TODO: click into time block to enter event
