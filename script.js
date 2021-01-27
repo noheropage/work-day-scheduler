42905af8cadb9206bbd48148ca8159a6de9b6ca6
noheropage <mursepage@gmail.com>
2021-01-26 22:06:39 -0800

    Update js scripts to include luxon for date/time feature; scaffolding main requirements

---
+++ script.js
@@ -0,0 +1,7 @@
+// TODO: display current day at top of calender
+
+// TODO: color coded time blocks to indicate past, present, or future
+
+// TODO: click into time block to enter event
+
+// TODO: save button for time block to save text to local storage
