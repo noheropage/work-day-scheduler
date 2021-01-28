11c459c8e7665c0f447c16331e84d5823aa0d325
noheropage <mursepage@gmail.com>
2021-01-28 00:20:30 -0800

    Prevents user input data from previous days from displaying on current day calendar

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 var DateTime = luxon.DateTime;
 var dt = DateTime.local()
 var now = DateTime.local()
-var test = dt.minus({hours: 12})
-console.log(test.hour);
+// var test = dt.minus({months: 2})
+// console.log(test.month);
 // TODO: add timer loop so clock updates automatically
 $("#currentDay").append(dt.toLocaleString(DateTime.DATETIME_MED))
 
-// TODO: color coded time blocks to indicate past, present, or future
+var storedMonth = JSON.parse(localStorage.getItem("storedMonth")) || dt.month
+var storedDay = JSON.parse(localStorage.getItem("storedDay")) || dt.day
+console.log(storedMonth);
+console.log(storedDay);
+
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 var militaryHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
@@ -25,9 +29,9 @@ for (var i=0; i < hours.length; i++) {
     newHour.addClass("hour col-1")
     // newHour.attr("id", hours[i])
     // console.log(newHour);
-    if (parseInt(militaryHours[i]) === test.hour) {
+    if (parseInt(militaryHours[i]) === dt.hour) {
         newInput.addClass("present description col-9 " + hours[i])
-    } else if (parseInt(militaryHours[i]) < test.hour) {
+    } else if (parseInt(militaryHours[i]) < dt.hour) {
         newInput.addClass("past description col-9 " + hours[i])
     } else {
         newInput.addClass("future description col-9 " + hours[i])
@@ -50,8 +54,15 @@ for (var i=0; i < hours.length; i++) {
     $(newRow).append(newSaveBtn)
     
     // look through local storage and display previous user input 
-    var oldInput = JSON.parse(localStorage.getItem("textInput" + i))
-    newInput.val(oldInput) 
+    if (dt.month !== storedMonth || dt.day !== storedDay) {
+        // console.log("not same day");
+        newInput.removeAttr("value")
+    } else {
+        var oldInput = JSON.parse(localStorage.getItem("textInput" + i))
+        newInput.val(oldInput) 
+        // console.log("same day");
+    }
+    
 }
 
 $(".saveBtn").on("click", function(event) {
@@ -64,5 +75,7 @@ $(".saveBtn").on("click", function(event) {
     
     // place it into local storage 
     localStorage.setItem("textInput" + $(this).data("index"), JSON.stringify(textInput))
+    localStorage.setItem("storedMonth", JSON.stringify(storedMonth))
+    localStorage.setItem("storedDay", JSON.stringify(storedDay))
 })
 
