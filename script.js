e7197a48892875e66e03c5844fabaf96e8ccfec7
noheropage <mursepage@gmail.com>
2021-01-27 23:55:43 -0800

    Change class of input field to color code as past, present, or future

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 var DateTime = luxon.DateTime;
 var dt = DateTime.local()
 var now = DateTime.local()
+var test = dt.minus({hours: 12})
+console.log(test.hour);
 // TODO: add timer loop so clock updates automatically
 $("#currentDay").append(dt.toLocaleString(DateTime.DATETIME_MED))
 
 // TODO: color coded time blocks to indicate past, present, or future
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var militaryHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
 var newRow;
 var newHour;
 var newInput;
 var newSaveBtn;
-var fullInput = [];
 // set up columns and rows for hours 9-5 
 for (var i=0; i < hours.length; i++) {
     newRow = $("<div>")
@@ -23,7 +25,14 @@ for (var i=0; i < hours.length; i++) {
     newHour.addClass("hour col-1")
     // newHour.attr("id", hours[i])
     // console.log(newHour);
-    newInput.addClass("future description col-9 " + hours[i])
+    if (parseInt(militaryHours[i]) === test.hour) {
+        newInput.addClass("present description col-9 " + hours[i])
+    } else if (parseInt(militaryHours[i]) < test.hour) {
+        newInput.addClass("past description col-9 " + hours[i])
+    } else {
+        newInput.addClass("future description col-9 " + hours[i])
+    }
+    
     newInput.attr("type", "text")
     // newInput.attr("")
     newSaveBtn.addClass("saveBtn col-2")
@@ -33,23 +42,18 @@ for (var i=0; i < hours.length; i++) {
     
     newHour.text(hours[i])
     newInput.val(newInput.textContent)
-    console.log(newInput.val());
+    // console.log(newInput.val());
     newSaveBtn.text("Save")
     $(".calendar").append(newRow)
     $(newRow).append(newHour)
     $(newRow).append(newInput)
     $(newRow).append(newSaveBtn)
-    // fullInput += newInput;
+    
+    // look through local storage and display previous user input 
     var oldInput = JSON.parse(localStorage.getItem("textInput" + i))
-    // console.log(oldInput);
     newInput.val(oldInput) 
-    // console.log(newInput.text());
 }
 
-
-// TODO: click into time block to enter event
-
-// TODO: save button for time block to save text to local storage
 $(".saveBtn").on("click", function(event) {
     // get text data from input field 
     event.preventDefault()
@@ -60,13 +64,5 @@ $(".saveBtn").on("click", function(event) {
     
     // place it into local storage 
     localStorage.setItem("textInput" + $(this).data("index"), JSON.stringify(textInput))
-
-    // display the info stored in local storage (first thing) 
-
 })
 
-// for (var i=0; i < hours.length; i++) {
-//     var oldInput = JSON.parse(localStorage.getItem("textInput" + i))
-//     console.log(oldInput);
-//     newInput.textContent = oldInput 
-// }
