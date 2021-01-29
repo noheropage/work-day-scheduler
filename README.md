a91100f373e5237892a7657da5aeaddddecc9ca8
noheropage <mursepage@gmail.com>
2021-01-29 00:12:44 -0800

    Cleaned up excess code and added comments

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Work-Day-Scheduler
 
 ## Description
-Website allows the user to input and save tasks corresponding with the various hours of a typical 9-5 workday. The date and time are displayed at top. If the website is loaded during the hours displayed the site will update the colors of the input field so the user can tell if the particular time block is past, present, or future. After entering text and hitting the save button a user can leave or refresh the page and keep the input data. If the user returns to the website on a new day the site will present empty input fields. 
+Website allows the user to input and save tasks corresponding with the various hours of a typical 9-5 workday with the help of the luxon api. The date and time are displayed at top. If the website is loaded during the hours displayed the site will update the colors of the input field so the user can tell if the particular time block is past, present, or future. After entering text and hitting the save button a user can leave or refresh the page and keep the input data. If the user returns to the website on a new day the site will present empty input fields. 
 
 ## Screenshot
-![screenshot after the hours shown in the scheduler] (./images/screenshot.png)
+![screenshot after the hours shown in the scheduler](images/screenshot.png)
+
 ## Link
 https://noheropage.github.io/work-day-scheduler/
--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 // display current day at top of calender
 var DateTime = luxon.DateTime;
 var dt = DateTime.local()
-var now = DateTime.local()
-// var test = dt.minus({months: 2})
-// console.log(test.month);
-// TODO: add timer loop so clock updates automatically
+
 $("#currentDay").append(dt.toLocaleString(DateTime.DATETIME_MED))
 
+// retrieve day and month from previous session to make sure tasks can be reset
 var storedMonth = JSON.parse(localStorage.getItem("storedMonth")) || dt.month
 var storedDay = JSON.parse(localStorage.getItem("storedDay")) || dt.day
-console.log(storedMonth);
-console.log(storedDay);
 
+// set up easily read hours for display and military time equivalent for comparisons with luxon
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 var militaryHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
-var newRow;
-var newHour;
-var newInput;
-var newSaveBtn;
 // set up columns and rows for hours 9-5 
 for (var i=0; i < hours.length; i++) {
-    newRow = $("<div>")
-    newHour = $("<div>")
-    newInput = $("<input>")
-    newSaveBtn = $("<button>")
+    var newRow = $("<div>")
+    var newHour = $("<div>")
+    var newInput = $("<input>")
+    var newSaveBtn = $("<button>")
     newRow.addClass("row time-block")
-    newHour.addClass("hour col-1")
-    // newHour.attr("id", hours[i])
-    // console.log(newHour);
+    
+    
+    // change color of input field depending on current time of day
     if (parseInt(militaryHours[i]) === dt.hour) {
         newInput.addClass("present description col-9 " + hours[i])
     } else if (parseInt(militaryHours[i]) < dt.hour) {
@@ -37,45 +30,38 @@ for (var i=0; i < hours.length; i++) {
         newInput.addClass("future description col-9 " + hours[i])
     }
     
-    newInput.attr("type", "text")
-    // newInput.attr("")
+    // set up attributes for save button 
     newSaveBtn.addClass("saveBtn col-2")
     newSaveBtn.attr("type", "submit")
     newSaveBtn.attr("data-index", i);
-    // console.log(newSaveBtn.data());
     
+    newHour.addClass("hour col-1")
     newHour.text(hours[i])
     newInput.val(newInput.textContent)
-    // console.log(newInput.val());
     newSaveBtn.text("Save")
+    // place new variables and their components into the html
     $(".calendar").append(newRow)
     $(newRow).append(newHour)
     $(newRow).append(newInput)
     $(newRow).append(newSaveBtn)
     
-    // look through local storage and display previous user input 
+    // look through local storage and display previous user input as long as it is from the same day/month
     if (dt.month !== storedMonth || dt.day !== storedDay) {
-        // console.log("not same day");
         newInput.removeAttr("value")
     } else {
         var oldInput = JSON.parse(localStorage.getItem("textInput" + i))
         newInput.val(oldInput) 
-        // console.log("same day");
     }
-    
 }
 
 $(".saveBtn").on("click", function(event) {
-    // get text data from input field 
+    // get text data from input field and prevent submit button from refreshing the page
     event.preventDefault()
-    // var element = event.
     // saves corresponding user input
     var textInput = $(this).parent().children(".description").val()
-    // console.log(textInput);
     
     // place it into local storage 
     localStorage.setItem("textInput" + $(this).data("index"), JSON.stringify(textInput))
     localStorage.setItem("storedMonth", JSON.stringify(storedMonth))
     localStorage.setItem("storedDay", JSON.stringify(storedDay))
 })
-
